5ca0113b0def0af87458765fab8a34294a266f76
Dale Bowles <dalebowles93@gmail.com>
2024-12-04 21:40:28 -0500

    added "if __name__ == '__main__'" check

--- headshots.py
+++ headshots.py
@@ -2,23 +2,24 @@ import os
 import requests as r
 import database as d
 
-roster = d.fetch_all("roster", "data/stats_2425.db")
-for p in roster:
-    name = p[2].replace(" ", "_").lower() + ".png"
-    headshot = p[1]
+if __name__ == '__main__':
+    roster = d.fetch_all("roster", "data/stats_2425.db")
+    for p in roster:
+        name = p[2].replace(" ", "_").lower() + ".png"
+        headshot = p[1]
 
-    files = os.listdir("assets/headshots")
-    if name in files:
-        continue
+        files = os.listdir("assets/headshots")
+        if name in files:
+            continue
 
-    with open(f"assets/headshots/{name}", 'wb') as handle:
-        response = r.get(headshot, stream=True)
+        with open(f"assets/headshots/{name}", 'wb') as handle:
+            response = r.get(headshot, stream=True)
 
-        if not response.ok:
-            print(response)
+            if not response.ok:
+                print(response)
 
-        for block in response.iter_content(1024):
-            if not block:
-                break
+            for block in response.iter_content(1024):
+                if not block:
+                    break
 
-            handle.write(block)
+                handle.write(block)
